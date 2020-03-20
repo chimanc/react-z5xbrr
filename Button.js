0d55cf80c2708fbe696e3ddb8bc74138efd910c0
chimanc <chimanc@outlook.com>
2020-03-20 18:07:40 +0800

    1 file modified

--- Button.js
+++ Button.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 export class Button extends React.Component {
   render() {
+    const stringProps = JSON.stringify(this.props);
     return (
-      <button onClick = {this.props.talk}>
-        Click me!
-      </button>
+      <div>
+        <h1>{stringProps}</h1>
+        <button onClick = {this.props.talk}>
+          Click me!
+        </button>
+      </div>
     );
   }
 }

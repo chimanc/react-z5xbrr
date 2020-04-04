2f462eafd6f539cc050557fe7e4a551a3d29f9e5
chimanc <chimanc@outlook.com>
2020-04-05 00:18:56 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+//Example 1//
 ReactDOM.render(
   <h1>{2 + 3}</h1>,
   document.getElementById('app')
 );
+
+//Example 2//
+/* 
+const math = (
+  <h1>
+    2 + 3 = {2 + 3}
+  </h1>;
+);
+
+ReactDOM.render(
+  <h1>{2 + 3}</h1>,
+  document.getElementById('app')
+);
+*/

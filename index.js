65ec4038b908d97503762d3cf7cb509b8ac829a5
chimanc <chimanc@outlook.com>
2020-04-05 00:30:27 +0800

    1 file modified

--- index.js
+++ index.js
@@ -2,72 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
 //Example 1//
-ReactDOM.render(
-  <h1>{2 + 3}</h1>,
-  document.getElementById('app')
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+const kitty = (
+	<img 
+		src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" 
+    onClick = {makeDoggy}
+    />
 );
 
-//Example 2//
-/* 
-const math = (
-  <h1>
-    2 + 3 = {2 + 3}
-  </h1>;
-);
+ReactDOM.render(kitty, document.getElementById('app'));
+////////////////
 
-ReactDOM.render(
-  <h1>{2 + 3}</h1>,
-  document.getElementById('app')
-);
-*/
-
-//Example 3//
-/*
-const theBestString = 'tralalalala i am da best';
-
-ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('app'));
-*/
-
-//Example 4//
-/*
-// Use a variable to set the `height` and `width` attributes:
-
-const sideLength = "200px";
-
-const panda = (
-  <img 
-    src="images/panda.jpg" 
-    alt="panda" 
-    height={sideLength} 
-    width={sideLength} />
-);
-*/
-
-//Example 5//
-/*
-const pics = {
-  panda: "http://bit.ly/1Tqltv5",
-  owl: "http://bit.ly/1XGtkM3",
-  owlCat: "http://bit.ly/1Upbczi"
-}; 
-
-const panda = (
-  <img 
-    src={pics.panda} 
-    alt="Lazy Panda" />
-);
-
-const owl = (
-  <img 
-    src={pics.owl} 
-    alt="Unimpressed Owl" />
-);
-
-const owlCat = (
-  <img 
-    src={pics.owlCat} 
-    alt="Ghastly Abomination" />
-); 
-*/

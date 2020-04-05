8446eb6175b01a9e7d5af5162dfaee3bb150188f
chimanc <chimanc@outlook.com>
2020-04-05 15:29:48 +0800

    1 file modified

--- index.js
+++ index.js
@@ -1,124 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const people = ['Rowe', 'Prevost', 'Gare'];
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+};
 
-
-//Example 1//
-/*
-const peopleLis = people.map(person =>
-
-  <li>{person}</li>);
-);
-*/
-
-//Example 2 KEY//
-
-const peopleLis = people.map((person, i) =>
-  // expression goes here:
-    <li key={'a_${i+1}'}>{person} {`a_${i+1}`} </li>
-  );
-
-// ReactDOM.render goes here:
 ReactDOM.render(
-  <ul>{peopleLis}</ul>,
+  <MyComponentClass />, 
   document.getElementById('app')
 );
-
-//NOTE: Templat Literal JS String can be `Hello ${1+2}!` and expected outcome would be Hello 3! 
-
-
-
-
-
-//Arrow Function//
-/*
-ar elements = [
-  'Hydrogen',
-  'Helium',
-  'Lithium',
-  'Beryllium'
-];
-
-// This statement returns the array: [8, 6, 7, 9]
-elements.map(function(element) {
-  return element.length;
-});
-
-// The regular function above can be written as the arrow function below
-elements.map((element) => {
-  return element.length;
-}); // [8, 6, 7, 9]
-
-// When there is only one parameter, we can remove the surrounding parentheses
-elements.map(element => {
-  return element.length;
-}); // [8, 6, 7, 9]
-
-// When the only statement in an arrow function is `return`, we can remove `return` and remove
-// the surrounding curly brackets
-elements.map(element => element.length); // [8, 6, 7, 9]
-
-// In this case, because we only need the length property, we can use destructuring parameter:
-// Notice that the `length` corresponds to the property we want to get whereas the
-// obviously non-special `lengthFooBArX` is just the name of a variable which can be changed
-// to any valid variable name you want
-elements.map(({ length: lengthFooBArX }) => lengthFooBArX); // [8, 6, 7, 9]
-
-// This destructuring parameter assignment can also be written as seen below. However, note that in
-// this example we are not assigning `length` value to the made up property. Instead, the literal name
-// itself of the variable `length` is used as the property we want to retrieve from the object.
-elements.map(({ length }) => length); // [8, 6, 7, 9] 
-
-
-
-
-//How Variable is declared//
-
-Refer to: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Object_destructuring
-
-let a, b, rest;
-[a, b] = [10, 20];
-console.log(a); // 10
-console.log(b); // 20
-
-[a, b, ...rest] = [10, 20, 30, 40, 50];
-console.log(a); // 10
-console.log(b); // 20
-console.log(rest); // [30, 40, 50]
-
-({ a, b } = { a: 10, b: 20 });
-console.log(a); // 10
-console.log(b); // 20
-
-
-// Stage 4(finished) proposal
-({a, b, ...rest} = {a: 10, b: 20, c: 30, d: 40});
-console.log(a); // 10
-console.log(b); // 20
-console.log(rest); // {c: 30, d: 40}
-
-
-
-//Expressions - Destructuring assignment
-const foo = ['one', 'two', 'three'];
-const [red, yellow, green] = foo;
-console.log(red); // "one"
-console.log(yellow); // "two"
-console.log(green); // "three"
-
-let a, b;
-
-[a, b] = [1, 2];
-console.log(a); // 1
-console.log(b); // 2
-
-
-const o = {p: 42, q: true};
-const {p: foo, q: bar} = o;
- 
-console.log(foo); // 42 
-console.log(bar); // true
-
-
-*/

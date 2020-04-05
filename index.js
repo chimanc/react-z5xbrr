487e2e5749bf6ea030853bb605345836881e747f
chimanc <chimanc@outlook.com>
2020-04-05 15:33:39 +0800

    1 file modified

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class MyComponentClass extends React.Component {
+  /*
+  A render method is a property whose name is render, and whose value is a function. The term “render method” can refer to the entire property, or to just the function part.
+  */
   render() {
     return <h1>Hello world</h1>;
   }
 };
+
+
 
 ReactDOM.render(
   <MyComponentClass />, 

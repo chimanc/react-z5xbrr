71fd89e3923c292c98191504a70a735ec82c5488
chimanc <chimanc@outlook.com>
2020-04-14 18:53:08 +0800

    1 file modified

--- index.js
+++ index.js
@@ -24,4 +24,22 @@ Every component has something called props.
 A component’s props is an object. It holds information about that component.
 To see a component’s props object, you use the expression this.props.
 
+
+How to make a component display passed-in information:
+
+1 - Find the component class that is going to receive that information.
+2 - Include this.props.name-of-information in that component class’s render method’s return statement.
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hi there, {this.props.firstName}</h1>;
+  }
+}
+
+ReactDOM.render(
+  <Greeting firstName='Groaaberta' />, 
+  document.getElementById('app')
+);
 */
